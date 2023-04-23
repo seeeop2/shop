5495d76494e55a24d271b13aef0126abc52536f1
seeeop2 <seeeop2@gmail.com>
2023-04-23 09:19:59 +0900

    2.6 Spring DATA JPA @Query 어노테이션

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +16,18 @@ public interface ItemRepository extends JpaRepository<Item,Long> {
     List<Item> findByPriceLessThan(Integer price);
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")  //SQL과 유사한 JPQL
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);       //다른 방법이 있지만, @Param을 추천
+
+    @Query(value = "select * from Item i where i.item_detail like %:itemDetail% order by i.price desc",nativeQuery = true)
+                                                                //value 안에 네이티브 쿼리문 작성하고 nativeQeury = true 작성해준다.
+                                                                //데이터베이스에 사용하던 쿼리를 그대로 사용해야 할 때는 nativeQeury속성을 사용하면
+                                                                //기존 쿼리 그대로 활용 가능
+                                                                //But 특정 DB에 종속되는 쿼리문을 사용하기 때문에 DB에 대한 독립적이라는 장점 잃는다.
+                                                                //기존에 작성한 통계용 쿼리처럼 복잡한 쿼리 그대로 사용해야 하는 경우 활용
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
+
+
 }
