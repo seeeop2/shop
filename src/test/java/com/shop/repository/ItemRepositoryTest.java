f324acc6735bb3cb7d39c5894e25743337d15f9a
inseop <seeeop2@gmail.com>
2023-04-24 12:23:48 +0900

    2.7 Spring DATA JPA Querydsl

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -1,13 +1,22 @@
 package com.shop.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
+import com.shop.entity.QItem;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -25,6 +34,10 @@ class ItemRepositoryTest {
 
     @Autowired                                                          //ItemRepository를 사용하기 위해 Bean 주입
     ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager em;                                                   //영속성 컨텍스트를 사용하기 위해 @PersistenceContext 어노테이션을
+                                                                        //이용해서 EntityManager Bean 주입
 
     @Test                                                               //테스트할 메소드 위에 선언하여 해당 메소드를 테스트 대상으로 지정
                                                                         //만약 여러 테스트가 있으면 @Ignore 사용해서 무시 가능
@@ -55,6 +68,34 @@ class ItemRepositoryTest {
             Item savedItem = itemRepository.save(item);
         }
     }
+
+    public void createItemList2(){                   //상품 데이터를 만드는 새로운 메소드를 하나 만듦
+                                                     //1~5 판매상태 SELL, 6~10 판매상태 SOLD_OUT 세팅
+        for(int i = 1 ; i <= 5;i++){
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+        for(int i = 6 ; i <= 10;i++){
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setStockNumber(0);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            itemRepository.save(item);
+        }
+
+    }
+
 
     @Test
     @DisplayName("상품명 조회 테스트")
@@ -117,4 +158,64 @@ class ItemRepositoryTest {
             System.out.println(item.toString());
         }
     }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest(){
+        this.createItemList();
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);         //JPAQueryFactory를 이용하여 쿼리를 동적으로 생성.
+                                                                        //생성자의 파라미터로 EntityManager 객체 투입
+        QItem qItem = QItem.item;                                       //Querydsl을 통해 쿼리를 생성하기 위해
+                                                                        //플러그인을 통해 자동으로 생성된 Qitem 객체 이용
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)           //자바 소스코드지만 SQL문과 비슷하게 소스를 작성할 수 있다
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%"+"테스트 상품 상세 설명"+"%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch();                            //JPAQeury 메소드 중 하나인 fetch를 이용해서 쿼리 결과를 리스트로 반환
+                                                                        //fetch() 메소드 실행 시점에 쿼리문 실행
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트 2")
+    public void queryDslTest2(){
+        this.createItemList2();
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();       //BooleanBuilder는 쿼리에 들어갈 조건을 만들어주는 빌더라고 생각
+                                                                    //Predicate를 구현하고 있으며, 메소드 체인 형식으로 사용
+        QItem item = QItem.item;
+
+        String itemDetail = "테스트 상품 상세 설명";
+        int price = 10003;
+        String itemSellStat = "SELL";
+
+        booleanBuilder.and(item.itemDetail.like("%" +itemDetail+"%"));  //필요한 상품을 조회하는데 필요한 "and"조건을 추가
+                                                                            //상품의 판매상태가 SELL일 때만 booleanBuilder에 판매상태 조건을 동적으로 추가
+        booleanBuilder.and(item.price.gt(price));
+
+        Pageable pageable = PageRequest.of(0,5);        //데이터를 페이징해 조회하도록 PageRequest.of() 메소드를 이용해
+                                                                  //Pageable 객체 생성. 첫 번째 인자는 조회할 페이지 번호,
+                                                                  //두 번째 인자는 한 페이지당 조회할 데이터의 개수
+        Page<Item> itemPagingResult =
+                itemRepository.findAll(booleanBuilder,pageable);  //QueryDslPredicateExecutor 인터페이스에서 정의한 findAll() 메소드를 이용해
+                                                                  //조건에 맞는 데이터를 Page 객체로 받아옴
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
